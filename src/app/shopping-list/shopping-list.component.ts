b74f3294c79100cfefa74b5f64bbe0896829c116
Jitgitbit <rowowolf@gmail.com>
2020-05-06 09:22:53 +0200

    shopping-list done

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 25),
+    new Ingredient('Potatoes', 10),
+    new Ingredient('Onions', 3),
+    new Ingredient('Watermelon', 1),
+  ];
 
   constructor() { }
 
