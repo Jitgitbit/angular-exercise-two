aae38c1b358aaea0065141b4569ae052beb4c590
Jitgitbit <rowowolf@gmail.com>
2020-05-08 14:20:41 +0200

    HC some real recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,26 +9,26 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
+    new Recipe('Croissant with soft Egg', 'Very nice breakfast', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
       new Ingredient('Egg', 1),
       new Ingredient('Croissant', 1),
       new Ingredient('Cherry Tomato', 2),
       new Ingredient('Rucola', 9),
       new Ingredient('Guacamole', 1),
     ]),
-    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
-      new Ingredient('Egg', 1),
-      new Ingredient('Croissant', 1),
-      new Ingredient('Cherry Tomato', 2),
-      new Ingredient('Rucola', 9),
-      new Ingredient('Guacamole', 1),
+    new Recipe('Tasty Schnitzel', 'The famous awesome german dish', 'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG', [
+      new Ingredient('Turkey Meat Filet', 1),
+      new Ingredient('Chappelure', 1),
+      new Ingredient('Lemon', 1),
+      new Ingredient('Belgian Fries', 20),
+      new Ingredient('Mustard', 1),
     ]),
-    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
-      new Ingredient('Egg', 1),
-      new Ingredient('Croissant', 1),
-      new Ingredient('Cherry Tomato', 2),
-      new Ingredient('Rucola', 9),
-      new Ingredient('Guacamole', 1),
+    new Recipe('Big Fat Burger', 'What else you need to say?', 'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg', [
+      new Ingredient('Bacon', 3),
+      new Ingredient('Angus Beef Hamburger', 1),
+      new Ingredient('Emmenthal Cheese', 2),
+      new Ingredient('Iceberg lettuce', 1),
+      new Ingredient('Mustard', 1),
     ]),
   ];
 
