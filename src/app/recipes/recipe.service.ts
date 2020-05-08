f07d31a40444a5968f849a57ef3103507bc725bd
Jitgitbit <rowowolf@gmail.com>
2020-05-08 13:39:55 +0200

    Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,33 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg'),
-    new Recipe('Test Recipe Two', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg'),
-    new Recipe('Test Recipe Three', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg'),
-    new Recipe('Test Recipe Four', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg')
+    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
+      new Ingredient('Egg', 1),
+      new Ingredient('Croissant', 1),
+      new Ingredient('Cherry Tomato', 2),
+      new Ingredient('Rucola', 9),
+      new Ingredient('Guacamole', 1),
+    ]),
+    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
+      new Ingredient('Egg', 1),
+      new Ingredient('Croissant', 1),
+      new Ingredient('Cherry Tomato', 2),
+      new Ingredient('Rucola', 9),
+      new Ingredient('Guacamole', 1),
+    ]),
+    new Recipe('Test Recipe One', 'This is simply a test', 'https://p0.pikrepo.com/preview/800/706/bread-with-tomato-and-green-vegetable-on-white-ceramic-plate.jpg', [
+      new Ingredient('Egg', 1),
+      new Ingredient('Croissant', 1),
+      new Ingredient('Cherry Tomato', 2),
+      new Ingredient('Rucola', 9),
+      new Ingredient('Guacamole', 1),
+    ]),
   ];
 
   getRecipes(){
