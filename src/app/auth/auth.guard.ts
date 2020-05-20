f30701a331bf703e44f029dd4009f4b343daac21
Jitgitbit <rowowolf@gmail.com>
2020-05-20 20:26:12 +0200

    using a UrlTree for redirecting with the Guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,6 +2,8 @@ import {
   CanActivate,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
+  UrlTree,
+  Router,
 } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -11,14 +13,24 @@ import { AuthService } from './auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     router: RouterStateSnapshot
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    return this.authService.user.pipe(map(user => {
-      return !!user;
-    }));
+  ):
+    | boolean
+    | UrlTree
+    | Promise<boolean | UrlTree>
+    | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      map((user) => {
+        const isAuth = !!user;
+        if (isAuth) {
+          return true;
+        }
+        return this.router.createUrlTree(['/auth']);
+      })
+    );
   }
 }
