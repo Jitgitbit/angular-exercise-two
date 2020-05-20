48a28bed389d787dc3e6b2e8a521d346b4447497
Jitgitbit <rowowolf@gmail.com>
2020-05-20 19:53:16 +0200

    Auto-Logout put to good use in the right places

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -29,6 +29,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    this.autoLogout(expiresIn * 1000);
     localStorage.setItem('userData', JSON.stringify(user));
   }
 
@@ -98,6 +99,8 @@ export class AuthService {
 
     if(loadedUser.token){
       this.user.next(loadedUser);
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();      //.getTime puts it into milliseconds
+      this.autoLogout(expirationDuration);
     }
   }
 
