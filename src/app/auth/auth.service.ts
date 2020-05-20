3e5ef5957f34e8b845ea1648c9434fa5f38bfb3a
Jitgitbit <rowowolf@gmail.com>
2020-05-20 18:17:14 +0200

    for better UX, redirecting to auth on logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 import { throwError, BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { User } from './user.model';
 
@@ -20,7 +21,7 @@ export class AuthService {
 
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient, private router: Router){}
 
   signup(email: string, password: string){
     return this.http.post<AuthResponseData>(
@@ -79,5 +80,6 @@ export class AuthService {
 
   logout(){
     this.user.next(null);
+    this.router.navigate(['/auth']);
   }
 }
