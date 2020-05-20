9d23aff6ab032c6e7e7635dd2ea4f056985e92e1
Jitgitbit <rowowolf@gmail.com>
2020-05-20 17:51:51 +0200

    ! using .clone() !!!

---
+++ src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@angular/core";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpParams } from '@angular/common/http';
+import { take, exhaustMap } from 'rxjs/operators';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor{
+  constructor(private authService: AuthService){}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler){
+    return this.authService.user.pipe(
+      take(1),                                               //----> here take will take 1 value from the observable, and then unsubscribe !
+      exhaustMap((user) => {
+        const modifiedReq = req.clone({
+          params: new HttpParams().set('auth', user.token)
+        });
+        return next.handle(req);
+      })
+    );
+  }
+}
