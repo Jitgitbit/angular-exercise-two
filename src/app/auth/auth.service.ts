7ee3aea668b8e34803a9a89203a584aec87f2150
Jitgitbit <rowowolf@gmail.com>
2020-05-20 12:44:42 +0200

    Creating & Storing the User Data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+
+import { User } from './user.model';
 
 export interface AuthResponseData {
   kind: string;
@@ -16,6 +18,8 @@ export interface AuthResponseData {
 @Injectable({providedIn: 'root'})
 export class AuthService {
 
+  user = new Subject<User>();
+
   constructor(private http: HttpClient){}
 
   signup(email: string, password: string){
@@ -26,8 +30,20 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));
   };
+
+  private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+
+    const user = new User(email, userId, token, expirationDate);
+
+    this.user.next(user);
+  }
+
   login(email: string, password: string){
     return this.http.post<AuthResponseData>(
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAciAldHViHThHcqsJ_wjQ6qLNMO06zQv4',
@@ -36,8 +52,11 @@ export class AuthService {
         password: password,
         returnSecureToken: true
       }
-    ).pipe(catchError(this.handleError));
+    ).pipe(catchError(this.handleError), tap(resData => {
+      this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+    }));
   }
+
   private handleError(errorRes: HttpErrorResponse){
     let errorMessage = 'An unknown error occured'                 //default error message
 
@@ -52,7 +71,7 @@ export class AuthService {
         errorMessage = 'This email is not registered!';
         break;
       case 'INVALID_PASSWORD':
-        errorMessage = 'Password is not valid!';
+        errorMessage = 'Password is not correct!';
         break;
     }
     return throwError(errorMessage);
