6bdc6b4f60f1275dd82549897249a922ad4c8ca0
Jitgitbit <rowowolf@gmail.com>
2020-05-21 15:09:03 +0200

    ! Lazy Loading ! ->  for auth and shopping-list -->  again big performance gain --->  bundle size is shrinking even more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,16 @@ const appRoutes: Routes = [
     path: 'recipes',
     loadChildren: () =>                                                        //=====> take heed of this Syntax !
       import('./recipes/recipes.module').then((m) => m.RecipesModule),
+  },
+  {
+    path: 'shopping-list',
+    loadChildren: () =>                                                        //=====> take heed of this Syntax !
+      import('./shopping-list/shopping-list.module').then((m) => m.ShoppingListModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>                                                        //=====> take heed of this Syntax !
+      import('./auth/auth.module').then((m) => m.AuthModule),
   }, 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 // import {RecipesModule} from './recipes/recipes.module'
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
+// import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
-import { AuthModule } from './auth/auth.module';
+// import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
@@ -21,8 +21,8 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AppRoutingModule,  
     // RecipesModule,
-    ShoppingListModule,
-    AuthModule,
+    // ShoppingListModule,
+    // AuthModule,
     SharedModule,
     CoreModule,
   ],
