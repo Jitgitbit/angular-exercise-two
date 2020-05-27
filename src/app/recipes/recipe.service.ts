91e823830f9dc57b28bb8ab7e0c4ad97a3db76d7
Jitgitbit <rowowolf@gmail.com>
2020-05-27 21:07:59 +0200

    Multiple Actions (--> AddIngredients)

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,6 +4,8 @@ import { Subject } from 'rxjs';
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Store } from '@ngrx/store';
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions';
 
 
 @Injectable()
@@ -55,7 +57,10 @@ export class RecipeService {
   //   ]),
   // ];
 
-  constructor(private slService: ShoppingListService){}
+  constructor(
+    private slService: ShoppingListService,
+    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>
+  ){}
 
   setRecipes(recipes: Recipe[]){
     this.recipes = recipes;
@@ -68,7 +73,8 @@ export class RecipeService {
     return this.recipes.slice()[index];
   }
   addIngredientsToShoppingList(ingredients: Ingredient[]){
-    this.slService.addIngredients(ingredients);
+    // this.slService.addIngredients(ingredients);
+    this.store.dispatch(new ShoppingListActions.AddIngredients(ingredients));
   }
   addRecipe(recipe: Recipe){
     this.recipes.push(recipe);
