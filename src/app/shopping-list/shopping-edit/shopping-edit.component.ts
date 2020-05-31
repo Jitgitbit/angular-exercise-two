997449e47833eb249a73abc9e787f3064a58e559
Jitgitbit <rowowolf@gmail.com>
2020-05-31 22:18:36 +0200

    Expanding the State + AppState

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -11,6 +11,8 @@ import { Store } from '@ngrx/store';
 import { ShoppingListService } from '../shopping-list.service';
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from '../store/shopping-list.actions';
+import * as fromShoppingList from '../store/shopping-list.reducer';
+
 
 @Component({
   selector: 'app-shopping-edit',
@@ -30,7 +32,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   constructor(
     private slService: ShoppingListService,
-    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+    private store: Store<fromShoppingList.AppState>
   ) {}
 
   ngOnInit(): void {
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -5,48 +5,63 @@ import { Ingredient } from '../../shared/ingredient.model';
 // import { ADD_INGREDIENT } from './shopping-list.actions';
 import * as ShoppingListActions from './shopping-list.actions';
 
-const initialState = {
+export interface AppState {
+  shoppingList: State;
+}
+
+export interface State {
+  ingredients: Ingredient[];
+  editedIngredient: Ingredient;
+  editedIngredientIndex: number;
+}
+
+const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 25),
     new Ingredient('Potatoes', 10),
     new Ingredient('Onions', 3),
     new Ingredient('Watermelon', 1),
-  ]
+  ],
+  editedIngredient: null,
+  editedIngredientIndex: -1, //------> because 0 is a valid index of course !
 };
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions){
-  switch (action.type){
+export function shoppingListReducer(
+  state: State = initialState,
+  action: ShoppingListActions.ShoppingListActions
+) {
+  switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
       return {
         ...state,
-        ingredients: [...state.ingredients, action.payload]
+        ingredients: [...state.ingredients, action.payload],
       };
     case ShoppingListActions.ADD_INGREDIENTS:
       return {
         ...state,
-        ingredients: [...state.ingredients, ...action.payload]         //----> using a spread operator, otherwise you would have a nested array !
+        ingredients: [...state.ingredients, ...action.payload], //----> using a spread operator, otherwise you would have a nested array !
       };
     case ShoppingListActions.UPDATE_INGREDIENT:
       const ingredient = state.ingredients[action.payload.index];
       const updatedIngredient = {
-        ...ingredient,                         //----> ...ingredient is there because we don't want to overwrite the id !
-        ...action.payload.ingredient
+        ...ingredient, //----> ...ingredient is there because we don't want to overwrite the id !
+        ...action.payload.ingredient,
       };
       const updatedIngredients = [...state.ingredients];
       updatedIngredients[action.payload.index] = updatedIngredient;
       return {
         ...state,
-        ingredients: updatedIngredients
+        ingredients: updatedIngredients,
       };
     case ShoppingListActions.DELETE_INGREDIENT:
       return {
         ...state,
         ingredients: state.ingredients.filter((ig, igIndex) => {
           return igIndex !== action.payload;
-        })        
+        }),
       };
     default:
       return state;
-  };
-};
+  }
+}
