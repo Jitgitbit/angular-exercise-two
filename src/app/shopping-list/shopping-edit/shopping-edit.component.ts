68d0d6cbcd703416a112bffa53a04c4ee835d49a
Jitgitbit <rowowolf@gmail.com>
2020-05-31 21:49:51 +0200

    Updating & Deleting Ingredients NgRx

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output, OnDestroy } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { ShoppingListService } from '../shopping-list.service';
-import {Ingredient} from '../../shared/ingredient.model'
+import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from '../store/shopping-list.actions';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit, OnDestroy {
   // @ViewChild('nameInput', {static:false}) nameInputRef: ElementRef;
   // @ViewChild('amountInput', {static:false}) amountInputRef: ElementRef;
   // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
   // @Output() ingredientAdded = new EventEmitter<Ingredient>();
-  @ViewChild('f', {static: false}) slForm: NgForm;
+  @ViewChild('f', { static: false }) slForm: NgForm;
   subscription: Subscription;
   editedItemIndex: number;
   editedItem: Ingredient;
@@ -25,47 +30,54 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
 
   constructor(
     private slService: ShoppingListService,
-    private store: Store<{shoppingList: {ingredients: Ingredient[]}}>
-  ) { }
+    private store: Store<{ shoppingList: { ingredients: Ingredient[] } }>
+  ) {}
 
   ngOnInit(): void {
     this.subscription = this.slService.startedEditing.subscribe(
       (index: number) => {
         this.editedItemIndex = index;
         this.editMode = true;
-        this.editedItem = this.slService.getIngredient(index); 
+        this.editedItem = this.slService.getIngredient(index);
         this.slForm.setValue({
           name: this.editedItem.name,
-          amount: this.editedItem.amount
+          amount: this.editedItem.amount,
         });
       }
     );
   }
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.subscription.unsubscribe();
   }
 
-  onSubmitAddOrUpdate(form: NgForm){
+  onSubmitAddOrUpdate(form: NgForm) {
     // const ingName = this.nameInputRef.nativeElement.value;
     // const ingAmount = this.amountInputRef.nativeElement.value;
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
 
-    if(this.editMode){
-      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
-    }else{
-      // this.slService.addIngredient(newIngredient); 
+    if (this.editMode) {
+      // this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(
+        new ShoppingListActions.UpdateIngredient({
+          index: this.editedItemIndex,
+          ingredient: newIngredient,
+        })
+      );
+    } else {
+      // this.slService.addIngredient(newIngredient);
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
-    }  
-    this.editMode = false;  // prevents button sticking on update after submit !
-    form.reset();  // resets textfields after submit 
+    }
+    this.editMode = false; // prevents button sticking on update after submit !
+    form.reset(); // resets textfields after submit
   }
-  onClear(){
+  onClear() {
     this.slForm.reset();
     this.editMode = false;
   }
-  onDelete(){
-    this.slService.deleteIngredient(this.editedItemIndex);
+  onDelete() {
+    // this.slService.deleteIngredient(this.editedItemIndex);
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex));
     this.onClear();
   }
 }
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -27,6 +27,25 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
         ...state,
         ingredients: [...state.ingredients, ...action.payload]         //----> using a spread operator, otherwise you would have a nested array !
       };
+    case ShoppingListActions.UPDATE_INGREDIENT:
+      const ingredient = state.ingredients[action.payload.index];
+      const updatedIngredient = {
+        ...ingredient,                         //----> ...ingredient is there because we don't want to overwrite the id !
+        ...action.payload.ingredient
+      };
+      const updatedIngredients = [...state.ingredients];
+      updatedIngredients[action.payload.index] = updatedIngredient;
+      return {
+        ...state,
+        ingredients: updatedIngredients
+      };
+    case ShoppingListActions.DELETE_INGREDIENT:
+      return {
+        ...state,
+        ingredients: state.ingredients.filter((ig, igIndex) => {
+          return igIndex !== action.payload;
+        })        
+      };
     default:
       return state;
   };
