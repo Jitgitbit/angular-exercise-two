f7ac3998aad57c4cf15247f847daff4f6a722e70
Jitgitbit <rowowolf@gmail.com>
2020-06-01 23:04:22 +0200

    auth  complete store added + folder cleanup

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,18 +1,28 @@
-import { Injectable } from "@angular/core";
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpParams } from '@angular/common/http';
-import { take, exhaustMap } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpParams
+} from '@angular/common/http';
+import { take, exhaustMap, map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { AuthService } from './auth.service';
+import * as fromApp from '../store/app.reducer';
 
 @Injectable()
-export class AuthInterceptorService implements HttpInterceptor{
-  constructor(private authService: AuthService){}
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService, private store: Store<fromApp.AppState>) {}
 
-  intercept(req: HttpRequest<any>, next: HttpHandler){
-    return this.authService.user.pipe(
-      take(1),                                               //----> here take will take 1 value from the observable, and then unsubscribe !
-      exhaustMap((user) => {
-        if(!user){
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return this.store.select('auth').pipe(
+      take(1),
+      map(authState => {
+        return authState.user;
+      }),
+      exhaustMap(user => {
+        if (!user) {
           return next.handle(req);
         }
         const modifiedReq = req.clone({
