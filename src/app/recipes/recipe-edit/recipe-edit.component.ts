a8426e2fb60a2a0f9f30e83e19a2547af2596903
Jitgitbit <rowowolf@gmail.com>
2020-06-06 01:11:31 +0200

    recipe edit component adapted

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,23 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 import { RecipeService } from '../recipe.service';
 import * as fromApp from '../../store/app.reducer';
+import * as RecipesActions from '../store/recipe.actions';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
 
-  get ingredientsControls() {                                                  // a getter! Sometimes Angular doesn't understand TS
+  private storeSub: Subscription;
+
+  get ingredientsControls() {                                                    // a getter! Sometimes Angular doesn't understand TS
     return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
 
@@ -31,33 +35,40 @@ export class RecipeEditComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
-      this.editMode = params['id'] != null;                          // nice trick to check truthy or falsy here !
+      this.editMode = params['id'] != null;                                        // nice trick to check truthy or falsy here !
       this.initForm();
     });
   }
 
   onSubmitAddOrUpdate() {
     console.log(`recipeForm says what?`,this.recipeForm);
-    // const newRecipe = new Recipe(                                          // GOOD EXAMPLE OF OUR REACTIVE APPROACH:
-    //   this.recipeForm.value['name'],                                      // Since our value of the form has exactly the same format and names
-    //   this.recipeForm.value['description'],                              // as our Recipe Model, you can skip the step of saving it into a new constant
-    //   this.recipeForm.value['imagePath'],                               //  Line 82 and 85 are the way !!!
+    // const newRecipe = new Recipe(                                           // GOOD EXAMPLE OF OUR REACTIVE APPROACH:
+    //   this.recipeForm.value['name'],                                       // Since our value of the form has exactly the same format and names
+    //   this.recipeForm.value['description'],                               // as our Recipe Model, you can skip the step of saving it into a new constant !
+    //   this.recipeForm.value['imagePath'],
     //   this.recipeForm.value['ingredients']);
     if (this.editMode) {
-      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+      // this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+      this.store.dispatch(
+        new RecipesActions.UpdateRecipe({
+          index: this.id,
+          newRecipe: this.recipeForm.value
+        })
+      );
     } else {
-      this.recipeService.addRecipe(this.recipeForm.value);
+      // this.recipeService.addRecipe(this.recipeForm.value);
+      this.store.dispatch(new RecipesActions.AddRecipe(this.recipeForm.value));
     }
     this.onCancelAndOrRedirectBack();
   }
 
   onAddIngredient() {
-    (<FormArray>this.recipeForm.get('ingredients')).push(                        // Explicitely casted into a FormArray here for demanding TS !!!
+    (<FormArray>this.recipeForm.get('ingredients')).push(                 // Explicitely casted into a FormArray here for demanding TS !!!
       new FormGroup({
         name: new FormControl(null, Validators.required),
         amount: new FormControl(null, [
           Validators.required,
-          Validators.pattern(/^[1-9]+[0-9]*$/)                                     // YEEY!! RegEx again !!
+          Validators.pattern(/^[1-9]+[0-9]*$/)                      // YEEY!! RegEx again !!
         ])
       })
     );
@@ -71,6 +82,12 @@ export class RecipeEditComponent implements OnInit {
     this.router.navigate(['../'], { relativeTo: this.route });
   }
 
+  ngOnDestroy() {
+    if (this.storeSub) {
+      this.storeSub.unsubscribe();
+    }
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
@@ -79,7 +96,7 @@ export class RecipeEditComponent implements OnInit {
 
     if (this.editMode) {
       // const recipe = this.recipeService.getRecipe(this.id);
-      this.store
+      this.storeSub = this.store
         .select('recipes')
         .pipe(
           map(recipeState => {
@@ -99,7 +116,7 @@ export class RecipeEditComponent implements OnInit {
                   name: new FormControl(ingredient.name, Validators.required),
                   amount: new FormControl(ingredient.amount, [
                     Validators.required,
-                    Validators.pattern(/^[1-9]+[0-9]*$/)                                       // YEEY!! RegEx again !!
+                    Validators.pattern(/^[1-9]+[0-9]*$/)                                      // YEEY!! RegEx again !!
                   ])
                 })
               );
