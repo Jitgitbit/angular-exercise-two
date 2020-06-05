b4f895ce94716501ad651ef4e87ad05cbb716c94
Jitgitbit <rowowolf@gmail.com>
2020-06-05 23:33:07 +0200

    recipe.effects done + data-storage adapted

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,42 +1,51 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { map, tap, take, exhaustMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map, tap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
-import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { AuthService } from '../auth/auth.service';
+import { RecipeService } from '../recipes/recipe.service';
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
   constructor(
     private http: HttpClient,
     private recipeService: RecipeService,
-    private authService: AuthService
+    private store: Store<fromApp.AppState>
   ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes();
     this.http
-      .put('https://phoenixrecipebook.firebaseio.com/recipes.json', recipes)
-      .subscribe((response) => {
-        console.log(`storeRecipes response says what?`, response);
+      .put(
+        'https://phoenixrecipebook.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe(response => {
+        console.log(response);
       });
   }
+
   fetchRecipes() {
     return this.http
-      .get<Recipe[]>('https://phoenixrecipebook.firebaseio.com/recipes.json')
+      .get<Recipe[]>(
+        'https://phoenixrecipebook.firebaseio.com/recipes.json'
+      )
       .pipe(
-        map((recipes) => {                             //----> this map is the rxjs operator
-          return recipes.map((recipe) => {                   //----> this map is the normal JS ArrayMethod
+        map(recipes => {                                                     //----> this map is the rxjs operator
+          return recipes.map(recipe => {                                          //----> this map is the normal JS ArrayMethod
             return {
               ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : [],
+              ingredients: recipe.ingredients ? recipe.ingredients : []
             };
           });
         }),
-        tap((recipes) => {
-          console.log(`fetchRecipes response says what?`, recipes);
-          this.recipeService.setRecipes(recipes);
+        tap(recipes => {
+          console.log(`fetchRecipes response says what?`, recipes);               //-----> check this out !
+          // this.recipeService.setRecipes(recipes);
+          this.store.dispatch(new RecipesActions.SetRecipes(recipes));
         })
       );
   }
